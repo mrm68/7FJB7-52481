8fc914c4761d5f2482cc3e3f9cd90d8a198189c7
reza <marzban40@gmail.com>
2025-02-08 10:57:43 +0100

    Test's written and validated, Documentation.txt written and appended

--- reservation/tests.py
+++ reservation/tests.py
@@ -1,54 +1,78 @@
-from django.urls import reverse
-from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework.test import APIClient, force_authenticate
 from rest_framework import status
-from .models import Reservation, Table
-from accounts.models import CustomUser
+from .models import Table, Reservation
 
-class ReservationURLTests(APITestCase):
-    def setUp(self):
-        # Create a user and log in
-        self.user = CustomUser.objects.create_user(username='testuser', password='testpassword')
-        self.client.login(username='testuser', password='testpassword')
-
-        # Create a table
-        self.table = Table.objects.create(seats=6, available=True)
-
-        # Create a reservation
-        self.reservation = Reservation.objects.create(
-            user=self.user,
-            table=self.table,
-            date="2023-10-15",
-            time="18:00",
-            guests=4
+class ReservationDetailTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret",
         )
-
-    def test_reservation_list_url(self):
-        # Get the URL for the reservation list
-        url = reverse('reservationList')
-
-        # Make a GET request to the reservation list URL
-        response = self.client.get(url)
-
-        # Check that the response status code is 200 (OK)
+        cls.other_user = get_user_model().objects.create_user(
+            username="otheruser",
+            email="other@email.com",
+            password="secret",
+        )
+        cls.table = Table.objects.create(seats=6, available=True)
+        cls.reservation = Reservation.objects.create(
+            user=cls.user,
+            table=cls.table,
+            seats_reserved=4,
+            cost=300,
+        )
+        cls.other_reservation = Reservation.objects.create(
+            user=cls.other_user,
+            table=cls.table,
+            seats_reserved=4,
+            cost=300,
+        )
+    
+    def setUp(self):
+        self.client = APIClient()
+    
+    # RETRIEVE OWN
+    def test_retrieve_own_reservation_detail(self):
+        """Test retrieving own reservation detail"""
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(f'/api/reservations/{self.reservation.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # Optionally, check that the response contains the reservation we created
-        self.assertEqual(len(response.data), 1)  # Assuming the response is a list of reservations
-        self.assertEqual(response.data[0]['id'], self.reservation.id)
-
-    def test_reservation_detail_url(self):
-        # Get the URL for the reservation detail view
-        url = reverse('reservationDetail', args=[self.reservation.id])
-
-        # Make a GET request to the reservation detail URL
-        response = self.client.get(url)
-
-        # Check that the response status code is 200 (OK)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # Check that the response contains the correct reservation data
-        self.assertEqual(response.data['id'], self.reservation.id)
-        self.assertEqual(response.data['table'], self.table.id)
-        self.assertEqual(response.data['date'], "2023-10-15")
-        self.assertEqual(response.data['time'], "18:00")
-        self.assertEqual(response.data['guests'], 4)
+        self.assertEqual(response.data['seats_reserved'], 4)
+        self.assertEqual(response.data['cost'], 300)
+    
+    # FORBIDDEN OTHER
+    def test_retrieve_other_user_reservation_detail(self):
+        """Test retrieving another user's reservation detail, should be not found or forbidden"""
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(f'/api/reservations/{self.other_reservation.id}/')
+        self.assertIn(response.status_code, [status.HTTP_403_FORBIDDEN, status.HTTP_404_NOT_FOUND])
+    
+    # DELETE OWN
+    def test_delete_own_reservation(self):
+        """Test deleting own reservation"""
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'/api/reservations/{self.reservation.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Reservation.objects.filter(id=self.reservation.id).count(), 0)
+    
+    # DELETE OTHER
+    def test_delete_other_user_reservation(self):
+        """Test deleting another user's reservation, should be not found or forbidden"""
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'/api/reservations/{self.other_reservation.id}/')
+        self.assertIn(response.status_code, [status.HTTP_403_FORBIDDEN, status.HTTP_404_NOT_FOUND])
+    
+    # UNAUTH RETRIEVE
+    def test_unauthenticated_user_cannot_retrieve_reservation_detail(self):
+        """Test unauthenticated user cannot retrieve reservation detail"""
+        response = self.client.get(f'/api/reservations/{self.reservation.id}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+    # UNAUTH DELETE
+    def test_unauthenticated_user_cannot_delete_reservation(self):
+        """Test unauthenticated user cannot delete reservation"""
+        response = self.client.delete(f'/api/reservations/{self.reservation.id}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
